b13ccbbb9ea8fe08a877cedf1e5b239813e509b4
gschiechedirik <gschiechedirik@gsd.pb.local>
2021-10-14 17:12:19 +0200

    Adding IPv6 support

--- check-net-config.sh
+++ check-net-config.sh
@@ -15,10 +15,12 @@ function ShowHelp {
         echo "Anwendung:"
         echo
         echo "$0 [-p|--pause [Anhalten zwischen den einzelnen Kommandos]] [-t|--targethost Zieladresse]"
+        echo "[--host6 [IPv6-Adresse als Zielhost]]"
+        echo "[-6|--IPv6 [IPv6-Verbindung prüfen]]"
         echo "[-h|--help [Anzeigen dieser Hilfe]]"
         echo
         echo "Als Ziele sollten öffentlich erreichbare IP-Adressen gewählt werden wie z. B." 
-        echo "217.160.86.33 (dcd.ionos.com)." 
+        echo "185.48.116.14 (dcd.ionos.com)." 
         echo
         echo "Die Option -p hält den Programmablauf an, damit die Ausgabe mittels Screenshot festghalten werden kann."
         echo
@@ -27,6 +29,8 @@ function ShowHelp {
         echo "Usage:"
         echo
         echo "$0 [-p|--pause [pause after each cammand execution]] [-t|--targethost host_to_test]" 
+        echo "[--host6 [us IPv6 address as target]]"
+        echo "[-6|--IPv6 [check IPv6 connectivity]]"
         echo "[-h|--help [print this help message]]"
         echo
         echo "As target host you should prefer a publicly reachable IP address, for" 
@@ -38,7 +42,9 @@ function ShowHelp {
     fi
 }
 
-TargetHost=185.48.116.14
+TargetHost4=185.48.116.14
+TargetHost6=2a02:247a::42:34
+
 ResultFile=/tmp/support_$(hostname)_$(date +%s).log
 
 while test $# -gt 0 ; do
@@ -50,8 +56,18 @@ while test $# -gt 0 ; do
             ShowHelp;
             exit ;; 
         -t|--targethost) shift;
-            TargetHost=$1;
-            if [[ "$TargetHost" == "" ]] ; then
+            TargetHost4=$1;
+            if [[ "$TargetHost4" == "" ]] ; then
+                ShowHelp
+                exit 2
+            fi ;
+            shift ;;
+        -6|--IPv6) 
+            IPv6=true
+            shift ;;
+        --host6) shift;
+            TargetHost6=$1;
+            if [[ "$TargetHost6" == "" ]] ; then
                 ShowHelp
                 exit 2
             fi ;
@@ -61,29 +77,52 @@ while test $# -gt 0 ; do
     esac
 done
 
-CommandList=(
-    "date"
-    "uname -a"
-    "cat /etc/os-release"
-    "ip neigh"
-    "ip address list"
-    "ip route show"
-    "ip neighbour show"
-    "iptables --list --numeric --verbose"
-    "cat /etc/sysconfig/network*/ifcfg-*"
-    "cat /etc/network/interfaces"
-    "cat /etc/resolv.conf"
-    "netstat --tcp --numeric -a"
-    "netstat --udp --numeric -a"
-    "cat /etc/hosts"
-    "ping -c 5 localhost"
-    "if which mtr > /dev/null ; then mtr -n -r $TargetHost ; else traceroute -M icmp $TargetHost ; fi"
-    "time nslookup $TargetHost"
-    "time nslookup ietf.com"
-    "ping -c 5 $TargetHost"
-)
+function CommandListIPv4 {
+    CommandList=(
+        "date"
+        "uname -a"
+        "cat /etc/os-release"
+        "iptables --list --numeric --verbose"
+        "cat /etc/sysconfig/network*/ifcfg-*"
+        "cat /etc/network/interfaces"
+        "cat /etc/resolv.conf"
+        "cat /etc/hosts"
+        "time nslookup $TargetHost4"
+        "time nslookup ietf.com"
+        "ip -4 neigh"
+        "ip -4 address list"
+        "ip -4 route show"
+        "ip -4 neighbour show"
+        "ss -4 --tcp --process --all --numeric"
+        "ss -4 --udp --process --all --numeric"
+        "ping -4 -c 5 $TargetHost4"
+        "ping -c 5 localhost"
+        "if which mtr > /dev/null ; then mtr -4 -n -r $TargetHost4 ; else traceroute -4 -M icmp $TargetHost4 ; fi"
+    )
+}
+
+function CommandListIPv6 {
+    CommandList=(
+        "ip -6 neigh"
+        "ip -6 address list"
+        "ip -6 route show"
+        "ip -6 neighbour show"
+        "ss -6 --tcp --process --all --numeric"
+        "ss -6 --udp --process --all --numeric"
+        "ip6tables --list --numeric --verbose"
+        "ping6 -c 5 localhost"
+        "if which mtr > /dev/null ; then mtr -6 -n -r $TargetHost6 ; else traceroute -6 -M icmp $TargetHost6 ; fi"
+        "time nslookup $TargetHost6"
+        "ping6 -c 5 $TargetHost6"
+    )
+}
 
 function CheckNetwork {
+    if [[ $1 == "4" ]] ; then 
+        CommandListIPv4
+    elif [[ $1 == "6" ]] ; then 
+        CommandListIPv6
+    fi 
     for i in $(seq 0 $((${#CommandList[*]}-1))) ; do 
         TopLine=$(echo ${CommandList[$i]} | tr '[:print:]' '=')
         echo
@@ -96,7 +135,11 @@ function CheckNetwork {
     done
 }
 
-CheckNetwork 2>&1 | tee $ResultFile
+CheckNetwork 4 2>&1 | tee -a $ResultFile
+
+if [[ ${IPv6} == "true" ]] || [[ ${TargetHost6} != "2a02:247a::42:34" ]] ; then
+    CheckNetwork 6 2>&1 | tee -a $ResultFile
+fi
 
 echo 
 
@@ -104,7 +147,7 @@ if [[ $LANG =~ de ]] ; then
    echo "Wenn Sie ein Supportticket eröffnen möchten, senden Sie bitte eine E-Mail an support@cloud.ionos.com"
    echo "und hängen Sie die Datei ${ResultFile} oder Screenshots der Kommandoausgaben an die E-Mail."
 else 
-   echo "If you would like to open a ticket for the ProfitBricks support, please write an e-mail to support@cloud.ionos.com"
+   echo "If you would like to open a ticket for the IONOS cloud support, please write an e-mail to support@cloud.ionos.com"
    echo "and attach the file ${ResultFile} or the screenshots of the command output to it."
 fi
 
